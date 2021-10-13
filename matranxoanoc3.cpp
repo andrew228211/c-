51eaa3a3ea9c0be07d1bbdacedc2dd49b47069ed
andrew228211 <andrew228211@gmail.com>
2021-10-13 07:58:23 +0700

    Create matranxoanoc3.cpp

---
+++ matranxoanoc3.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+using namespace std;
+main() {
+	int t;
+	cin>>t;
+	while(t--) {
+		int n,m,x;
+		cin>>n>>m>>x;
+		int a[n][m];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				cin>>a[i][j];
+			}
+		}
+		int b[n*m];
+		int hang=n,cot=m,k=0,p=0;
+		while(k<m*n)
+		{
+			for(int i=p;i<cot;i++)
+			{
+				b[k++]=a[p][i];
+			}
+			for(int i=p+1;i<hang;i++)
+			{
+				b[k++]=a[i][cot-1];
+			}
+			for(int i=cot-2;i>=p;i--)
+			{
+				b[k++]=a[hang-1][i];
+			}
+			for(int i=hang-2;i>p;i--)
+			{
+				b[k++]=a[i][p];
+			}
+			p++;
+			hang--;
+			cot--;
+		}
+		cout<<b[x-1];
+		cout<<endl;
+	}
+}
+
